36b2b2c45275fb00e23db5e6b3a2c26810e7c664
dannyloumax <dannyloumax@gmail.com>
2023-05-23 22:11:11 +0300

    uncommented

--- FactorialCalculator/src/Main.java
+++ FactorialCalculator/src/Main.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите факториал натурально числа n : ");
-        int n = scan.nextInt();
-
-        SuperFC superFC = new SuperFC();
-        int result = superFC.calculateFactorial(5);
-        FactorialCalculator factorialCalculator = new FactorialCalculator();
-        System.out.println(result);
-
-//        Car bmw = new Car();
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Введите факториал натурально числа n : ");
+//        int n = scan.nextInt();
 //
-//        bmw.showSpeed();
+//        SuperFC superFC = new SuperFC();
+//        int result = superFC.calculateFactorial(5);
+//        FactorialCalculator factorialCalculator = new FactorialCalculator();
+//        System.out.println(result);
+
+        Car bmw = new Car();
+
+        bmw.showSpeed();
     }
 }
